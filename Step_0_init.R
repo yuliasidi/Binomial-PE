0a6e5499cac51ffc4d4080986ce6dbcf37489681
Yulia Sidi <yulia.sidi@uconn.edu>
2018-12-24 15:53:02 -0500

    number of MI imputatitons added

--- Step_0_init.R
+++ Step_0_init.R
@@ -9,6 +9,7 @@ library(dplyr, warn.conflicts = F, quietly = T)
 library(ggplot2, warn.conflicts = F, quietly = T)
 library(purrr, warn.conflicts = F, quietly = T)
 library(reshape2, warn.conflicts = F, quietly = T)
+library(mice, warn.conflicts = F, quietly = T)
 
 source("PE_Bin_PD_Functions.R")
 
@@ -17,3 +18,7 @@ beta <- c(0.1)
 
 # # of simulated studies
 n.sim <- 10000
+
+# # of imputations for MI
+num.mi=5
+
