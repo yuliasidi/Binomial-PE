bf65438327da522952c340484d65555c51faa1ad
Yulia Sidi <yulia.sidi@uconn.edu>
2019-04-30 15:09:07 -0400

    updated

--- cluster/pgms/full_hpc_build.R
+++ cluster/pgms/full_hpc_build.R
@@ -14,7 +14,7 @@ purrr::pwalk(.l = sc.l,
              .f = function(scenario, do.val){
                cat(
                  whisker::whisker.render(
-                   readLines('cluster/pgms/fullH0_hpc.tmpl'),
+                   readLines('cluster/pgms/tmpls/fullH0_hpc.tmpl'),
                    data = list(scenario = scenario,
                                val = do.val)
                  ),
@@ -25,21 +25,60 @@ purrr::pwalk(.l = sc.l,
              })
 
 
-sc.n <- 
+sc.n1 <- 
   expand.grid(
-    scenario = seq(1,17,2),
-    do.val = seq(0.1,0.20,0.1),
-    setn = seq(1,4,1),
-    stringsAsFactors = FALSE)
+    scenario = 21,
+    do.val = c(0.1, 0.2),
+    setn = 1,
+    rho.c = c("p30","m30"),
+    stringsAsFactors = FALSE)%>%
+  mutate(rho = case_when(rho.c == "p30" ~ 0.3,
+                             rho.c == "m30" ~ -0.3))
 
-sc.n.l <- as.list(sc.n)
+purrr::pwalk(.l = as.list(sc.n1%>%filter(rho.c =="m30")),
+             .f = function(scenario, do.val, setn, rho, rho.c){
+               cat(
+                 whisker::whisker.render(
+                   readLines('cluster/pgms/tmpls/fullH0_hpc_setn1.tmpl'),
+                   data = list(scenario = scenario,
+                               val = do.val,
+                               setn = setn, 
+                               rval = rho,
+                               rc = rho.c)
+                 ),
+                 file = file.path('cluster/pgms/wald/setn/m30',
+                                  sprintf("fullH0_%d_do%d_set%d_m30.R", scenario, round(100*do.val,0), setn)
+                 ),
+                 sep='\n') 
+             })
 
-
-purrr::pwalk(.l = sc.n.l,
+purrr::pwalk(.l = as.list(sc.n1),
              .f = function(scenario, do.val, setn){
                cat(
                  whisker::whisker.render(
-                   readLines('cluster/pgms/tmpls/fullH0_hpc_setn.tmpl'),
+                   readLines('cluster/pgms/tmpls/fullH1_hpc_setn1.tmpl'),
+                   data = list(scenario = scenario,
+                               val = do.val,
+                               setn = setn)
+                 ),
+                 file = file.path('cluster/pgms/wald/setn',
+                                  sprintf("fullH1_%d_do%d_set%d.R", scenario, round(100*do.val,0), setn)
+                 ),
+                 sep='\n') 
+             })
+
+sc.n2 <- 
+  expand.grid(
+    scenario = 21,
+    do.val = c(0.1, 0.2),
+    setn = 2,
+    stringsAsFactors = FALSE)
+
+purrr::pwalk(.l = as.list(sc.n2),
+             .f = function(scenario, do.val, setn){
+               cat(
+                 whisker::whisker.render(
+                   readLines('cluster/pgms/tmpls/fullH0_hpc_setn2.tmpl'),
                    data = list(scenario = scenario,
                                val = do.val,
                                setn = setn)
@@ -49,7 +88,6 @@ purrr::pwalk(.l = sc.n.l,
                  ),
                  sep='\n') 
              })
-
 
 
 
@@ -105,3 +143,97 @@ purrr::pwalk(.l = sc.nomiss.l,
                  ),
                  sep='\n') 
              })
+
+
+##############
+### m=5
+##############
+sc.n1 <- 
+  expand.grid(
+    scenario = seq(1,30,1),
+    do.val = c(0.1, 0.2),
+    setn = 1,
+    rho.c = c("p30","m30"),
+    stringsAsFactors = FALSE)%>%
+  mutate(rho = case_when(rho.c == "p30" ~ 0.3,
+                         rho.c == "m30" ~ -0.3))%>%
+  mutate(bound = case_when(rho.c == "p30" ~ 'u',
+                           rho.c == "m30" ~ 'l'))
+
+
+purrr::pwalk(.l = as.list(sc.n1%>%filter(rho.c =="p30")),
+             .f = function(scenario, do.val, setn, rho, rho.c, bound){
+               cat(
+                 whisker::whisker.render(
+                   readLines('cluster/pgms/tmpls/fullH0_hpc_m5_setn1.tmpl'),
+                   data = list(scenario = scenario,
+                               val = do.val,
+                               setn = setn, 
+                               rval = rho,
+                               rc = rho.c,
+                               ulb = bound)
+                 ),
+                 file = file.path('cluster/pgms/wald/setn/mi5/p30',
+                                  sprintf("fullH0_%d_do%d_set%d_p30_mi5.R", scenario, round(100*do.val,0), setn)
+                 ),
+                 sep='\n') 
+             })
+
+
+purrr::pwalk(.l = as.list(sc.n1%>%filter(rho.c =="m30")),
+             .f = function(scenario, do.val, setn, rho, rho.c, bound){
+               cat(
+                 whisker::whisker.render(
+                   readLines('cluster/pgms/tmpls/fullH0_hpc_m5_setn1.tmpl'),
+                   data = list(scenario = scenario,
+                               val = do.val,
+                               setn = setn, 
+                               rval = rho,
+                               rc = rho.c,
+                               ulb = bound)
+                 ),
+                 file = file.path('cluster/pgms/wald/setn/mi5/m30',
+                                  sprintf("fullH0_%d_do%d_set%d_m30_mi5.R", scenario, round(100*do.val,0), setn)
+                 ),
+                 sep='\n') 
+             })
+
+
+purrr::pwalk(.l = as.list(sc.n1%>%filter(rho.c =="p30")),
+             .f = function(scenario, do.val, setn, rho, rho.c, bound){
+               cat(
+                 whisker::whisker.render(
+                   readLines('cluster/pgms/tmpls/fullH1_hpc_m5_setn1.tmpl'),
+                   data = list(scenario = scenario,
+                               val = do.val,
+                               setn = setn, 
+                               rval = rho,
+                               rc = rho.c,
+                               ulb = bound)
+                 ),
+                 file = file.path('cluster/pgms/wald/setn/mi5/p30',
+                                  sprintf("fullH1_%d_do%d_set%d_p30_mi5.R", scenario, round(100*do.val,0), setn)
+                 ),
+                 sep='\n') 
+             })
+
+
+purrr::pwalk(.l = as.list(sc.n1%>%filter(rho.c =="m30")),
+             .f = function(scenario, do.val, setn, rho, rho.c, bound){
+               cat(
+                 whisker::whisker.render(
+                   readLines('cluster/pgms/tmpls/fullH1_hpc_m5_setn1.tmpl'),
+                   data = list(scenario = scenario,
+                               val = do.val,
+                               setn = setn, 
+                               rval = rho,
+                               rc = rho.c,
+                               ulb = bound)
+                 ),
+                 file = file.path('cluster/pgms/wald/setn/mi5/m30',
+                                  sprintf("fullH1_%d_do%d_set%d_m30_mi5.R", scenario, round(100*do.val,0), setn)
+                 ),
+                 sep='\n') 
+             })
+
+
