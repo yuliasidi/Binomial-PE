983421976083c0624a0d6a0b925c319032afa98c
Yulia Sidi <yulia.sidi@uconn.edu>
2020-01-23 15:42:37 -0500

    ss plot updated

--- cluster/checks/ss.per.method.R
+++ cluster/checks/ss.per.method.R
@@ -6,14 +6,14 @@ ss <- readRDS("cluster/ss.bounds.rds")
 
 ss.plot <-
   ss%>%
-  dplyr::mutate(flabel = sprintf('Delta:%s', M2))%>%
+  dplyr::mutate(flabel = sprintf('M2:%s', M2))%>%
   ggplot(aes(x=p_C, y = n.arm, fill = method)) +
   geom_bar(position = "dodge",
            stat = "identity") +
   theme_bw() +
   facet_wrap(~flabel, labeller = ggplot2::label_parsed) +
   theme(legend.position = "bottom") +
-  scale_fill_discrete(labels=c('FM','Wald','WN')) + 
+  scale_fill_discrete(labels=c('FM','Wald','NW')) + 
   labs(x = "Event probability in control group",
        y = "N per arm",
        fill = "Method") +
